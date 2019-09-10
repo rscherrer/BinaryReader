446a99e7d1290e9227fb8364cce6c9893d13b619
rscherrer <raphael.scherrer@evobio.eu>
2019-09-10 16:20:18 +0200

    removed junk

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-import struct
 import numpy as np
 
-print(struct.pack('4f', 1.0, 2.0, 3.0, 4.0))
-print(np.frombuffer(b'\x00\x00\x80?\x00\x00\x00@\x00\x00@@\x00\x00\x80@', np.float32))
-
-with open("record.dat", "rb") as binary_file:
+with open("../build-EGS_test-kit-Debug/popsize1.dat", "rb") as binary_file:
     # Read the whole file at once
     data = binary_file.read()
     print(data)
