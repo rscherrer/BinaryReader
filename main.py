2a27e793881b91175746f8483a73f0a1071ab817
rscherrer <raphael.scherrer@evobio.eu>
2019-09-11 10:10:21 +0200

    removed useless file and useless output

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 
 import numpy as np
 
-with open("../build-EGS_test-kit-Debug/popsize1.dat", "rb") as binary_file:
+with open("../build-EGS_test-kit-Debug/resource11.dat", "rb") as binary_file:
     # Read the whole file at once
     data = binary_file.read()
-    print(data)
 
 print(np.frombuffer(data, np.float64))
